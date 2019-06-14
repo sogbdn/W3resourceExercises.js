a0c5a4ddc5bc60ef2763f29bcca8d0f4b408afcb
sogbdn <sonia.barretdenazaris@gmail.com>
2019-06-14 19:32:08 -0400

    added filter exercise

--- W3fundamentalJS.js
+++ W3fundamentalJS.js
@@ -30,3 +30,12 @@ var clipboard = new Clipboard('.copy-button', {
 const digitize = (n) => n.toString().split('').map(Number);
 
 console.log(digitize(12345));
+
+//8. Write a JavaScript program to filter out the specified values from an specified array.
+// Return the original array without the filtered values.
+
+const result = (arr, ...val) => {
+	val.forEach((v) => (arr = arr.filter((elt) => elt != v)));
+	return arr;
+};
+console.log(result([ 1, 2, 3, 4, 5 ], 1));
