e9d665c50ddf917a0059b4c51d15c3db103b32a4
sogbdn <sonia.barretdenazaris@gmail.com>
2019-06-28 15:30:55 -0400

    how to find gcd of 2 nb?

--- W3recursion.js
+++ W3recursion.js
@@ -11,3 +11,19 @@ console.log(calculateFactorial(2));
 console.log(calculateFactorial(3));
 console.log(calculateFactorial(4));
 console.log(calculateFactorial(5));
+
+// 2.  2. Write a JavaScript program to find the greatest
+// common divisor (gcd) of two positive numbers.
+var gcd2Number = (n1, n2) => {
+	min = Math.min(n1, n2);
+
+	if (n1 % min == 0 && n2 % min == 0) {
+		return min;
+	}
+	return gcd2Number(n1 - 1, n2 - 1);
+};
+
+console.log(gcd2Number(2, 4));
+console.log(gcd2Number(4, 12));
+console.log(gcd2Number(5, 25));
+console.log(gcd2Number(36, 6));
