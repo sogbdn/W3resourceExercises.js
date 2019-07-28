045e4e7c27448d6c9b5d73dcf0f3c277e62755c3
sogbdn <sonia.barretdenazaris@gmail.com>
2019-07-28 18:25:59 -0400

    Added function to sort arrary

--- W3ObjectExercises.js
+++ W3ObjectExercises.js
@@ -91,3 +91,29 @@ function calculatePerimeter(r) {
 
 console.log(calculateAera(3));
 console.log(calculatePerimeter(3));
+
+// 10. Write a JavaScript program to sort an array of JavaScript objects.
+
+function objArrSorted(arr) {
+	return arr.sort(function(a, b) {
+		return b.libraryID - a.libraryID;
+	});
+}
+var library = [
+	{
+		title: 'The Road Ahead',
+		author: 'Bill Gates',
+		libraryID: 1254
+	},
+	{
+		title: 'Walter Isaacson',
+		author: 'Steve Jobs',
+		libraryID: 4264
+	},
+	{
+		title: 'Mockingjay: The Final Book of The Hunger Games',
+		author: 'Suzanne Collins',
+		libraryID: 3245
+	}
+];
+console.log(objArrSorted(library));
